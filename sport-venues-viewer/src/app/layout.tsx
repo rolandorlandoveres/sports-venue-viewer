6134337eac7f04a68ff083127b8c2e286fbf8206
roland.veres <roland.veres@bca.com>
2025-10-20 16:22:46 +0300

    create project, download assets and change font

---
+++ sport-venues-viewer/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Josefin_Sans } from "next/font/google";
+import "./globals.css";
+
+const josephine = Josefin_Sans({
+  variable: "--font-josephine",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Sports venue viewer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josephine.variable} $antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
