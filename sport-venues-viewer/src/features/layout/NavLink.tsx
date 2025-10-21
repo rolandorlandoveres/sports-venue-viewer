c078a407040c46df0a598231be60e36f6c0cef0f
rolandorlandoveres <roland.veres@globallogic.com>
2025-10-21 19:56:57 +0300

    update nav link stylings

--- sport-venues-viewer/src/features/layout/NavLink.tsx
+++ sport-venues-viewer/src/features/layout/NavLink.tsx
@@ -18,7 +18,7 @@ export function NavLink({
   return (
     <Link
       href={href}
-      className={`p-1 text-xl font-[700] ${isActive ? 'border-b-primary border-b-4' : ''}`}
+      className={`after:bg-primary hover:after:bg-primary-hover relative pb-3 after:absolute after:bottom-0 after:left-1/2 after:h-[8px] after:-translate-x-1/2 after:transition-all after:duration-300 after:content-[''] hover:after:w-2/5 hover:after:after:-translate-x-1/2 hover:after:transition-all hover:after:duration-300 ${isActive ? 'after:w-2/5' : ''}`}
     >
       {children}
     </Link>
