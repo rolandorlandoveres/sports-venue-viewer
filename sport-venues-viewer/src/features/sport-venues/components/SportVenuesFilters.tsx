6b99b4915cc17e35338a739109c88ec21e2f212e
rolandorlandoveres <roland.veres@globallogic.com>
2025-10-22 14:47:29 +0300

    add  search on paste functionality, refactor

--- sport-venues-viewer/src/features/sport-venues/components/SportVenuesFilters.tsx
+++ sport-venues-viewer/src/features/sport-venues/components/SportVenuesFilters.tsx
@@ -1,19 +1,27 @@
 import { FilterInput } from '@/features/ui/FilterInput';
-import { useState, KeyboardEvent, ChangeEvent } from 'react';
+import { useState, KeyboardEvent, ChangeEvent, ClipboardEvent } from 'react';
 import {
   filterVenues,
   VenuesFilterActionPayload,
   useAppDispatch,
+  useAppSelector,
 } from '@sport-venues/store';
 
+function getTextFromClipboardEvent(e: ClipboardEvent<HTMLInputElement>) {
+  return e.clipboardData.getData('Text');
+}
+
 export function SportVenuesFilter() {
+  const filteredVenuesLength = useAppSelector(
+    (state) => state.sportVenues.filteredSportVenues.length,
+  );
   const dispatch = useAppDispatch();
 
   const [city, setCityFilter] = useState('');
   const [tag, setTagFilter] = useState('');
 
-  function filter(action: VenuesFilterActionPayload) {
-    dispatch(filterVenues(action));
+  function filter(action: Partial<VenuesFilterActionPayload>) {
+    dispatch(filterVenues({ city, tag, ...action }));
   }
 
   function filterOnHandleKeyDown(event: KeyboardEvent<HTMLInputElement>): void {
@@ -28,7 +36,7 @@ export function SportVenuesFilter() {
     const newValue = e.target.value;
     setCityFilter(newValue);
     if (newValue.trim().length === 0) {
-      filter({ city: newValue, tag });
+      filter({ city: newValue });
     }
   }
 
@@ -36,7 +44,7 @@ export function SportVenuesFilter() {
     const newValue = e.target.value;
     setTagFilter(newValue);
     if (newValue.trim().length === 0) {
-      filter({ city, tag: newValue });
+      filter({ tag: newValue });
     }
   }
 
@@ -44,15 +52,24 @@ export function SportVenuesFilter() {
     filter({ city, tag });
   }
 
+  function onCityPasteCapture(e: ClipboardEvent<HTMLInputElement>): void {
+    filter({ city: getTextFromClipboardEvent(e) });
+  }
+
+  function onTagPasteCapture(e: ClipboardEvent<HTMLInputElement>): void {
+    filter({ tag: getTextFromClipboardEvent(e) });
+  }
+
   return (
     <div className='border-separation-border border-t-1 border-b-1'>
-      <div className='ml-primary my-primary flex flex-wrap gap-5'>
+      <div className='ml-primary my-primary flex flex-wrap items-center gap-5'>
         <FilterInput
           placeholder='Filter by city'
           value={city}
           onChange={onCityChange}
           onKeyDown={filterOnHandleKeyDown}
           onIconClick={filterOnIconClick}
+          onPasteCapture={onCityPasteCapture}
         />
 
         <FilterInput
@@ -61,7 +78,10 @@ export function SportVenuesFilter() {
           onChange={onTagChange}
           onKeyDown={filterOnHandleKeyDown}
           onIconClick={filterOnIconClick}
+          onPasteCapture={onTagPasteCapture}
         />
+
+        <p className='m-0 p-0'>{filteredVenuesLength} Result(s)</p>
       </div>
     </div>
   );
